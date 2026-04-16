29608472a9aa36e45849b0fdbf40104d435bd8aa
Programmer1234536 <Antix.leb@outlook.com>
2026-04-16 20:41:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -371,8 +371,4 @@ def reject(id):
 # ---------- START ----------
 if __name__ == "__main__":
     init()
-    socketio.run(
-        app,
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000))
-    )
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
